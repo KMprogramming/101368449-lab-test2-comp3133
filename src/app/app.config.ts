f3d07121ad51c5b1fc15cfe080505d7afbf4cbad
KMprogramming <macayankarl24@gmail.com>
2024-04-25 11:09:12 -0400

    Config files fixed!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-<<<<<<< HEAD
+
 import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -16,20 +16,6 @@ export const appConfig: ApplicationConfig = {
     // Provide client hydration
     provideClientHydration(),
     provideHttpClient(withFetch())
-=======
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes'; // Update the import statement
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http'; // Import withFetch
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(withFetch()), // Use withFetch
-    provideRouter(routes),
-    provideClientHydration(),
-    provideAnimationsAsync()
->>>>>>> 0effcecf7b82d55c891014998116a6ce47795d79
   ]
 };
